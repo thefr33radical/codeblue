9e2d4c450297c66efea4407f452882908749d002
kuliza287 <imperial.gauntlet@gmail.com>
2018-03-15 16:36:22 +0530

    march15 18

---
+++ algo_ds/merge_Sort.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+void sorter(vector<int>&arr,int low,int mid,int high){
+	int start1=low,start2=mid+1;
+	vector<int>temp;
+		for(auto i:arr)
+		cout<<i<<" ";
+		cout<<endl;
+	while(start1<=mid && start2 <=high)
+	{	if(arr[start1]<arr[start2])
+		{	temp.push_back(arr[start1]);
+			start1+=1;
+		}
+		else
+			{	temp.push_back(arr[start2]);
+			start2+=1;
+		}
+	}
+	
+	while(start1<=mid)
+	{	temp.push_back(arr[start1]);
+		start1+=1;
+	}
+	while(start2 <=high)
+	{	temp.push_back(arr[start2]);
+		start2+=1;
+	}
+	int k=0;
+	for(auto i=low;i<=high;i++)
+		arr[i]=temp[k++];
+		
+	// Mistake when copying back copy arr[low]~arr[high]=temp[0]~temp[size]
+		
+	for(auto i:arr)
+		cout<<i<<" ";
+		
+	cout<<" low: "<<low<<" mid "<<mid<<" high "<<high<<endl ;
+	
+}
+
+
+void merge(vector<int>& arr,int low,int high)
+{	if(low<high)
+	{	int mid=(low+(high-1))/2;
+		merge(arr,low,mid);
+		merge(arr,mid+1,high);
+		sorter(arr,low,mid,high);
+	
+	}
+}
+
+
+int main() {
+	// your code goes here
+	vector<int>arr={1,4,3,6,6,3,8,23,4};
+	
+	merge(arr,0,arr.size());
+	for(auto i:arr)
+		cout<<i;
+
+	
+	
+	return 0;
+} 
--- merge_sort_strings.py
+++ merge_sort_strings.py
@@ -1,48 +1,56 @@
-
+string="123657842234"
 def sort(string,low,mid,high):
-    start1=low
-    start2=mid
+    start1=int(low)
+    start2=int(mid)
     temp=[]
+    mid=int(mid)
+    high=int(high)
     
     while(start1<mid and start2<high):
-        if(string[start1]<string[start2]):
+        if(string[start1]<(string[start2])):
             temp.append(string[start1])
-            start11+=start1
+          #  print(string[start1],"<",(string[start2]))
+          #  print(temp)
+            start1+=1
         else:
             temp.append(string[start2])
-            start2+=start2
-            
-        while(start1<mid):
+          #  print(string[start2],"<",(string[start1]))
+          #  print(temp)
+            start2+=1
+              
+    while(start1<mid):
             temp.append(string[start1])
-            start11+=start1
-        while(start2<high):
+            start1+=1
+    while(start2<high):
             temp.append(string[start2])
-            start2+=start2
-            
-    string =temp
+            start2+=1
+
+    
+    if(len(temp)>0):
+       string =temp
     return string
-    
+
 def split(string,low,high):
-    if(low<high):
+    if(int(low)<int(high)):
         mid=(low+high)/2
+        mid=int(mid)
+       
         split(string,low,mid)
+     
         split(string,mid+1,high)
-        sort(string,low,mid,high)
+        string=sort(string,low,mid,high)
         return string
-    
-    
-    
 
 def compute(string):
-    split(string,0,len(string))
-    
-
-
+    print(split(string,0,len(string)))
 
 if __name__=="__main__":
-    string="abcdefgh"
+    #string="124356387"
     l=list(string)
+    l=list(map(int,l))
     #print(l)
-    a=compute(l)
+    #print(l)
+    compute(l)
+   # print(string)
 
 
