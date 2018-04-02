d4f911551d24edd685b6fdd5026b65782f220137
kuliza287 <imperial.gauntlet@gmail.com>
2018-04-02 18:41:03 +0530

    2april18

---
+++ svm.py
@@ -0,0 +1,4 @@
+'''Given a string, calculate number of indexes such that the substring to left of the index 
+and the substring to the right of the index contains characters that can form the string 
+“programmer” .'''
+
