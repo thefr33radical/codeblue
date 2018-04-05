2590fded24870761f85f6c7227c74634fb97df40
Gowtham Nayak <imperial.gauntlet@gmail.com>
2018-04-05 16:08:06 +0530

    Delete svm.py

--- svm.py
+++
@@ -1,4 +0,0 @@
-'''Given a string, calculate number of indexes such that the substring to left of the index 
-and the substring to the right of the index contains characters that can form the string 
-“programmer” .'''
-
