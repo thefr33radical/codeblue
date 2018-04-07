e7ca7e9e41c7fd46efccc5a4feca8464a40d678a
Gowtham Nayak <imperial.gauntlet@gmail.com>
2018-04-07 23:49:11 +0530

    Add files via upload

---
+++ algo_ds/substring.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  1 16:16:02 2016
+
+@author: compaq
+"""
+
+x=raw_input("Enter the string1: ") 
+y=raw_input("Enter the string2: ")
+
+if y  in x:
+    print "string exists"
+else:
+    print "dosen't exist"
