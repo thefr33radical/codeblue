2b0ec682eddb11d10f4f8263194d3fd8881f0b2e
kuliza287 <imperial.gauntlet@gmail.com>
2018-04-11 12:35:05 +0530

    11april8

--- algo_ds/preorder_traversal.py
+++ algo_ds/preorder_traversal.py
@@ -1,5 +1,51 @@
 
-class node:
+#Preorder traversal for binary tree
+'''class node:
     def __init__(self):
         self.val=0
-        self.pointer=[]
+        self.left=None
+        self.right=None
+        
+    def add_node_left(self,val):
+        self.left=node()
+        self.val=val
+    
+    def add_node_right(self,val):
+        self.right=node()
+        self.val=val
+        
+    def printer(self,node):
+        temp=node
+        print(temp.val)
+        if(temp.left):
+            self.printer(temp.left)
+        if(temp.right):
+            self.printer(temp.right)
+        
+def compute():
+    t=node()
+    t.add_node_left(10)
+    t.add_node_right(20)
+   
+    root=t
+    t.left=node()
+    t.add_node_left(30)
+    t.right=node()
+    t.add_node_right(40)
+    
+    t=root
+    t.left=node()
+    t.add_node_left(50)
+    t.right=node()
+    t.add_node_right(60)
+    
+    root.printer(t)
+    
+    pass
+
+
+if __name__=="__main__":
+    compute()
+    
+    
+    '''
