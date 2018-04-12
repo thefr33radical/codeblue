8ddb313f13857febb8fd13af97be447c1de835e5
kuliza287 <imperial.gauntlet@gmail.com>
2018-04-12 23:57:06 +0530

    12april18

--- algo_ds/graphs/count_no_of_nodes.py
+++ algo_ds/graphs/count_no_of_nodes.py
@@ -1,61 +1,59 @@
 '''
 https://www.geeksforgeeks.org/count-number-nodes-given-level-using-bfs/
 
+#Tree
+T(C)=O(E+V)
+
 '''
 from collections import defaultdict
-
 class graph(object):
-    def __init__(self):
-        self.v=defaultdict(list)
+    def __init__(self,vertex):
+        self.adj=defaultdict(list)
+        self.ver=vertex
         
-def add_edge(obj,src,dest):
-    obj.v[src].append(dest)
-    obj.v[dest].append(src)
-    pass
+def add_edge(obj,u,v):
+        obj.adj[u].append(v)
         
-def bfs(obj):
+def count(obj,src,x):
     q=[]
-    visited=[False]*(len(obj.v)+1)
-    
-    src=0
-    dest=5
-    
-    visited[0]=True
-    q.append(src)
-    level=0
-    
-    
+    q.append([src,0])
     t=[]
-    t.append([src,level])
+    t.append([src,0])
     while(q):
-        cur_vertex=q.pop(0)
-        if(cur_vertex==dest):
-            break
+        temp=q.pop(0)
+        v=temp[0]
+        level=temp[1]
         level+=1
-        for i in obj.v[cur_vertex]:
-            if(visited[i]==False):
-                t.append([i,level])
-                temp_vertex=i
-                visited[i]=True
-                q.append(temp_vertex)
-    
+        
+        for i in obj.adj[v]:
+            q.append([i,level])
+            t.append([i,level])
     print(t)
+    
+    count=0
+    for i in t:
+        if(i[1]==x):
+            count+=1
+    print(count)
+            
+        
 
 def compute():
-    obj=graph()
-    
+    obj=graph(5)
+    level_count=2
     add_edge(obj,0, 1)
     add_edge(obj,0, 2)
-    add_edge(obj,1, 2)
+    add_edge(obj,0, 3)
+    #add_edge(obj,1, 2)
     #add_edge(obj,2, 0)
-    add_edge(obj,2, 3)
-    add_edge(obj,2, 7)
+   # add_edge(obj,2, 3)
+    add_edge(obj,3, 4)
     add_edge(obj,4, 8)
-    add_edge(obj,2, 4)
-    add_edge(obj,0, 5)
+    add_edge(obj,2, 5)
+    #add_edge(obj,0, 5)
     
-    print(len(obj.v))
-    bfs(obj)
+    print((obj.ver))
+    count(obj,0,level_count)
     ##print(i,obj.v[i])
 if __name__=="__main__":
     compute()
