996cda09318dbcd0bf212e3ffec861f5ba08df7e
kuliza287 <imperial.gauntlet@gmail.com>
2018-04-26 15:12:40 +0530

    28april18

---
+++ algo_ds/print_without_rec2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class t{
+int i=0;
+public:
+t()
+{
+
+cout<<i++<<" ";
+}
+
+};
+
+
+int main()
+{
+t obj[100];
+return 0;
+}
---
+++ algo_ds/print_without_recursion.cpp
@@ -0,0 +1,16 @@
+//print 100 nos without using loops/recursion
+#include<iostream>
+using namespace std;
+
+int a =0;
+#define A cout<<a++<<endl;
+#define B A A A A
+#define C B B B B B
+#define D C C C C C
+
+int main()
+{
+
+	D
+	return 0;
+}
