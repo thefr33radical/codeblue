c5709ba60a089d003ca86e05c43eed2ca2472d76
kuliza287 <imperial.gauntlet@gmail.com>
2018-05-04 16:30:50 +0530

    4May18

--- algo_ds/graphs/lowest_common_ancestor.py
+++ algo_ds/graphs/lowest_common_ancestor.py
@@ -1,3 +1,4 @@
+
 class tree(object):
     def __init__(self,v):
         self.left=None
@@ -8,33 +9,72 @@ def add_node(obj,left,right):
         obj.left=left
         obj.right=right
         
-def find(obj,val,stck1):
-    
-    find(obj.left,val)
-    pass
+def find_path(obj,val,q):
+    found=0
+    if(obj is None):
         
-def lca(obj,parent,val1,val):
-    stck1=[]
-    stck2=[]
+        return 0
+    if(obj.val==val):
+        return 1
+    q.append(obj)
     
-    find(obj.left,val1,stck1.append(obj))
-    
-    
-   
+    found=find_path(obj.left,val,q)
+    if(found==0):
+        found=find_path(obj.right,val,q)
+    else:
+        return 1
+        
+    if(found==0):
+        q.pop()
+        return 0
+        
+    else:
+        return 1
     
         
+def lca(obj,val1,val):
+    q1=[]
+    q2=[]
+    found1=0
+    found2=0
     
+    found1=find_path(obj,val1,q1)
+    found2=find_path(obj,val,q2)
+    '''
+    for i in q1:
+        print(i.val)
+    print('---')
+    for i in q2:
+        print(i.val)
         
-   return parent
+        '''
         
+    while(len(q1)>0 and len(q2)>0):
+        
+            if(q1[0]==q2[0]):
+                temp=q1.pop(0)
+                q2.pop(0)
+            else:
+                break
+            
+    print(temp.val)
+
 def compute():
-    obj=tree(1)
-    obj1=tree(2)
-    obj2=tree(3)
-    obj3=tree(4)
-    obj4=tree(5)
-    obj5=tree(6)  
-    obj6=tree(7)  
+    obj=tree(0)
+    obj1=tree(1)
+    obj2=tree(2)
+    obj3=tree(3)
+    obj4=tree(4)
+    obj5=tree(5)  
+    obj6=tree(6) 
+    
+    obj7=tree(7)
+    obj8=tree(8)  
+    obj9=tree(9)  
+    
+    obj10=tree(10)
+    obj11=tree(11)  
+    obj12=tree(12)  
         
     
     obj.left=obj1
@@ -45,3 +85,16 @@ def compute():
     
     obj2.left=obj5
     obj2.right=obj6
+    
+    
+    obj5.right=obj7
+    obj7.left=obj8
+    obj8.left=obj9
+    obj8.right=obj10
+    obj10.left=obj11
+    obj11.right=obj12
+    
+    lca(obj,9,12)
+    
+if __name__=="__main__":
+    compute()
