076d3cd0c1156ae181b7d22c8d605f2a1667e905
kuliza287 <imperial.gauntlet@gmail.com>
2018-05-23 17:02:58 +0530

    23may18

--- projects/accuracy_tuning_methods/models/models_with_kfold_rfe_kfold_hypertuning.py
+++ projects/accuracy_tuning_methods/models/models_with_kfold_rfe_kfold_hypertuning.py
@@ -2,8 +2,13 @@ from sklearn import svm
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures as poly
-import traceback
 import pandas as pd
+import traceback
+from sklearn import datasets
+from sklearn.feature_selection import RFE
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
 
 class Compute(object):
 
@@ -16,256 +21,168 @@ class Compute(object):
         self.path = ""
 
     def split_dataset(self):
-            # Split training data into training and test set
-        self.train_x = self.dataset.iloc[:int(0.8 * len(self.dataset)),
-                                         :4]
-        self.train_x.append(self.dataset.iloc[:int(0.8
-                                                   * len(self.dataset)), 5:])
-        self.test_x = self.dataset.iloc[int(0.8 * len(self.dataset)):
-                                        len(self.dataset), :4]
-        self.test_x.append(self.dataset.iloc[int(
-            0.8 * len(self.dataset)):len(self.dataset), 6:])
-        self.train_y = self.dataset.iloc[:int(0.8 * len(self.dataset)),
-                                         4]
-        self.test_y = self.dataset.iloc[:int(0.2 * len(self.dataset)),
-                                        4]
-        print (self.train_x, self.test_x, self.train_y, self.test_y)
+        # Split training data into training and test set
+        self.train_x = self.dataset.iloc[:-20, :10]
+
+        self.test_x = self.dataset.iloc[-20:, :10]
+        self.train_y = self.dataset.iloc[:-20, -1]
+        self.test_y = self.dataset.iloc[-20:, -1]
+
         return
 
-   
     def data_loader(self):
         try:
-            self.path = input()
-            self.dataset = pd.read_csv(
-                self.path,
-                index_col=None,
-                names= [
-                'Status of existing checking account',
-                'Duration in month',
-                'Credit history',
-                'Purpose',
-                'Credit amount',
-                'Savings account/bonds',
-                'Present employment since',
-                'Installment rate',
-                'Personal status and sex',
-                'Other debtors guarantors ',
-                'Present residence since ',
-                'Property ',
-                'Age in years ',
-                'Other installment plans ',
-                'Housing ',
-                'Number of existing credits at this bank',
-                'Job',
-                'Number of people being liable to provide maintenance',
-                'Telephone ',
-                'foreign worker',
-                ],
-                sep='  ',
-                engine="python")
-            # visual=proc.Process(target=self.data_visualization)
-            # visual.start()
-            # self.data_preprocessing()
-            
-            self.split_dataset()
+            diabetes = datasets.load_diabetes()
 
+            self.dataset = pd.DataFrame(diabetes["data"])
+            self.dataset.columns = diabetes["feature_names"]
+            target = pd.DataFrame(diabetes["target"], columns=["target"])
+            self.dataset = pd.concat([self.dataset, target], axis=1)
+
+            self.split_dataset()
             return (self.train_x, self.test_x, self.train_y,
                     self.test_y)
         except Exception:
             traceback.print_exc()
-            return (-1, -1, -1, -1)
+            return (None, None, None, None)
 
 
 class Algorithms(object):
-    
-    def __init__(self,
-        model_name,
-        train_x,
-        train_y,
-        test_x,
-        test_y):
-        self.elasticnet(model_name,
-        train_x,
-        train_y,
-        test_x,
-        test_y)
-        
-        self.svr(model_name,
-        train_x,
-        train_y,
-        test_x,
-        test_y)
-        
-        self.linear(model_name,
-        train_x,
-        train_y,
-        test_x,
-        test_y)
-        
-        self.polynomial(model_name,
-        train_x,
-        train_y,
-        test_x,
-        test_y)
-        
-        self.lasso(model_name,
-        train_x,
-        train_y,
-        test_x,
-        test_y)
-        
 
-    def svr(
-        self,
-        model_name,
-        train_x,
-        test_x,
-        train_y,
-        test_y,
-    ):
+    def __init__(self, model_name, train_x, test_x, train_y, test_y):
+
+        if model_name == "elasticnet":
+            self.elasticnet(model_name, train_x, test_x, train_y, test_y)
+
+        if model_name == "svr":
+            self.svr(model_name, train_x, test_x, train_y, test_y)
+
+        if model_name == "linear":
+            self.linear(model_name, train_x, test_x, train_y, test_y)
+
+        if model_name == "polynomial":
+            self.polynomial(model_name, train_x, test_x, train_y, test_y)
+
+        if model_name == "lasso":
+            self.lasso(model_name, train_x, test_x, train_y, test_y)
+
+    def rfe(self, train_x, test_x, train_y, test_y):
+        pred = []
+        temp_pred = []
+        final_error = 999999
+        count_of_features = len(train_x.columns)
+        n_features = []
+        feature_rank = []
+        temp_train_x = []
+        temp_test_x = []
+
+        models = [RandomForestRegressor(), linear_model.LinearRegression(), GradientBoostingRegressor()]
+        model_name = ""
+        x = []
+        for model in models:
+            for i in range(1, count_of_features):
+                selector = RFE(model, i, step=1)
+                selector = selector.fit(train_x, train_y)
+
+                temp_pred = selector.predict(test_x)
+                mean_error = mean_squared_error(test_y, temp_pred)
+
+                if mean_error < final_error:
+                    final_error = mean_error
+                    pred = temp_pred
+                    model_name = ""
+                    model_name = str(model)
+                    n_features = selector.n_features_
+                    feature_rank = selector.ranking_
+                    temp_train_x = selector.transform(train_x)
+                    temp_test_x = selector.transform(test_x)
+        print("Feature Mean error: ", final_error, n_features, feature_rank)
+        feature_labels = list(zip(feature_rank, train_x.columns))
+        feature_labels_rank = []
+        count = 0
+        for i in feature_labels:
+            temp = list(i)
+            temp.append(count)
+            feature_labels_rank.append(temp)
+            count += 1
+
+        feature_rank = pd.DataFrame(feature_labels_rank, columns=["Rank", "Label", "Index"])
+        # feature_rank.to_csv("feature_rank.csv")
+        print(feature_rank)
+
+        test_x = pd.DataFrame(temp_test_x)
+        train_x = pd.DataFrame(temp_train_x)
+        # print(test_x)
+        return train_x, test_x
+
+    def svr(self, model_name, train_x, test_x, train_y, test_y):
         model = svm.SVR(kernel='rbf')
+        train_x, test_x = self.rfe(train_x, test_x, train_y, test_y)
         model.fit(train_x, train_y)
         pred = model.predict(test_x)
-        self.print_score(
-            model_name,
-            train_y,
-            train_x,
-            test_y,
-            pred,
-            model,
-        )
+        self.print_score(model_name, train_y, train_x, test_y, pred, model)
 
-    def polynomial(
-        self,
-        model_name,
-        train_x,
-        test_x,
-        train_y,
-        test_y,
-    ):
+    def polynomial(self, model_name, train_x, test_x, train_y, test_y):
         model = linear_model.LinearRegression()
+
+        train_x, test_x = self.rfe(train_x, test_x, train_y, test_y)
+
         temp = poly(degree=2)
         train_x_poly = temp.fit_transform(train_x)
         test_x_poly = temp.fit_transform(test_x)
 
         model2 = model.fit(train_x_poly, train_y)
         pred2 = model2.predict(test_x_poly)
-        self.print_score(
-            model_name,
-            train_y,
-            train_x_poly,
-            test_y,
-            pred2,
-            model2,
-        )
+        self.print_score(model_name, train_y, train_x, test_y, pred2, model)
 
-    def linear(
-        self,
-        model_name,
-        train_x,
-        test_x,
-        train_y,
-        test_y,
-    ):
+    def linear(self, model_name, train_x, test_x, train_y, test_y):
         model = linear_model.LinearRegression()
+        train_x, test_x = self.rfe(train_x, test_x, train_y, test_y)
         model.fit(train_x, train_y)
         pred = model.predict(test_x)
-        self.print_score(
-            model_name,
-            train_y,
-            train_x,
-            test_y,
-            pred,
-            model,
-        )
+        self.print_score(model_name, train_y, train_x, test_y, pred, model)
 
-    def ridge(
-        self,
-        model_name,
-        train_x,
-        test_x,
-        train_y,
-        test_y,
-    ):
+    def ridge(self, model_name, train_x, test_x, train_y, test_y):
         model = linear_model.Ridge(alpha=3)
+        train_x, test_x = self.rfe(train_x, test_x, train_y, test_y)
         model.fit(train_x, train_y)
         pred = model.predict(test_x)
-        self.print_score(
-            model_name,
-            train_y,
-            train_x,
-            test_y,
-            pred,
-            model,
-        )
+        self.print_score(model_name, train_y, train_x, test_y, pred, model)
 
-    def lasso(
-        self,
-        model_name,
-        train_x,
-        test_x,
-        train_y,
-        test_y,
-    ):
+    def lasso(self, model_name, train_x, test_x, train_y, test_y):
         model = linear_model.Lasso()
+        train_x, test_x = self.rfe(train_x, test_x, train_y, test_y)
         model.fit(train_x, train_y)
         pred = model.predict(test_x)
-        self.print_score(
-            model_name,
-            train_y,
-            train_x,
-            test_y,
-            pred,
-            model,
-        )
+        self.print_score(model_name, train_y, train_x, test_y, pred, model)
 
-    def elasticnet(
-        self,
-        model_name,
-        train_x,
-        test_x,
-        train_y,
-        test_y,
-    ):
+    def elasticnet(self, model_name, train_x, test_x, train_y, test_y):
         model = linear_model.ElasticNet(alpha=1)
+        train_x, test_x = self.rfe(train_x, test_x, train_y, test_y)
         model.fit(train_x, train_y)
         pred = model.predict(test_x)
-        self.print_score(
-            model_name,
-            train_y,
-            train_x,
-            test_y,
-            pred,
-            model,
-        )
+        self.print_score(model_name, train_y, train_x, test_y, pred, model)
 
-    def print_score(
-        self,
-        model_name,
-        train_y,
-        train_x,
-        test_y,
-        pred,
-        model,
-    ):
+    def print_score(self, model_name, train_y, train_x, test_y, pred, model):
         try:
-            print ('Mean squared error' + model_name + ' : ',
-                   mean_squared_error(test_y, pred))
-            print ('R squared:  ' + model_name + ' : ',
-                   r2_score(test_y, pred))
+            print(len(pred), len(test_y))
+            print(model_name + " Mean Squared Error: ", mean_squared_error(test_y, pred))
+            print(model_name + " R2 Score : ", r2_score(test_y, pred))
         except Exception:
             traceback.print_exc()
+
 
 if __name__ == '__main__':
     obj = Compute()
     (train_x, test_x, train_y, test_y) = obj.data_loader()
-    models = [
-            "svr",
-            "lasso",
-            "elasticnet",
-            "ridge",
-            "linear",
-            "polynomial",
-            ]
-    for i in models:
-        obj2=Algorithms(i,train_x, test_x, train_y, test_y)
+    # print(len(train_x), len(train_y), len(test_x), len(test_y))
+    # obj2 = Algorithms("linear", train_x, test_x, train_y, test_y)
+    obj2 = Algorithms("polynomial", train_x, test_x, train_y, test_y)
+
+    ''' TEST CASES
+
+    '''
+
+
+
+
