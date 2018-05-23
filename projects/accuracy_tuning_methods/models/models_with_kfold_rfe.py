108693b06e79826f5571af32d05c19e904b035df
kuliza287 <imperial.gauntlet@gmail.com>
2018-05-23 15:11:43 +0530

    23may18

--- projects/accuracy_tuning_methods/models/models_with_kfold_rfe.py
+++ projects/accuracy_tuning_methods/models/models_with_kfold_rfe.py
@@ -19,22 +19,22 @@ class Compute(object):
 
     def split_dataset(self):
         # Split training data into training and test set
-        self.train_x = self.dataset.iloc[:-20 , :10]
+        self.train_x = self.dataset.iloc[:-20, :10]
 
         self.test_x = self.dataset.iloc[-20:, :10]
-        self.train_y = self.dataset.iloc[:-20 ,-1]
+        self.train_y = self.dataset.iloc[:-20, -1]
         self.test_y = self.dataset.iloc[-20:, -1]
-        #print(self.test_y)
+        # print(self.test_y)
         return
 
     def data_loader(self):
         try:
-            diabetes=datasets.load_diabetes()
+            diabetes = datasets.load_diabetes()
 
-            self.dataset=pd.DataFrame(diabetes["data"])
-            self.dataset.columns=diabetes["feature_names"]
-            target = pd.DataFrame( diabetes["target"], columns=["target"] )
-            self.dataset=pd.concat( [ self.dataset , target], axis=1 )
+            self.dataset = pd.DataFrame(diabetes["data"])
+            self.dataset.columns = diabetes["feature_names"]
+            target = pd.DataFrame(diabetes["target"], columns=["target"])
+            self.dataset = pd.concat([self.dataset, target], axis=1)
 
             self.split_dataset()
             return (self.train_x, self.test_x, self.train_y,
@@ -52,22 +52,22 @@ class Algorithms(object):
                  test_x,
                  train_y,
                  test_y):
-        if model_name=="elasticnet":
+        if model_name == "elasticnet":
             self.elasticnet(model_name,
-                        train_x,
-                        test_x,
-                        train_y,
-                        test_y)
+                            train_x,
+                            test_x,
+                            train_y,
+                            test_y)
 
         if model_name == "svr":
             self.svr(model_name,
-                        train_x,
-                        test_x,
-                        train_y,
-                        test_y)
+                     train_x,
+                     test_x,
+                     train_y,
+                     test_y)
 
         if model_name == "linear":
-                self.linear(model_name,
+            self.linear(model_name,
                         train_x,
                         test_x,
                         train_y,
@@ -75,17 +75,17 @@ class Algorithms(object):
 
         if model_name == "polynomial":
             self.polynomial(model_name,
-                        train_x,
-                        test_x,
-                        train_y,
-                        test_y)
+                            train_x,
+                            test_x,
+                            train_y,
+                            test_y)
 
         if model_name == "lasso":
             self.lasso(model_name,
-                        train_x,
-                        test_x,
-                        train_y,
-                        test_y)
+                       train_x,
+                       test_x,
+                       train_y,
+                       test_y)
 
     def svr(
             self,
@@ -98,7 +98,7 @@ class Algorithms(object):
         model = svm.SVR(kernel='rbf')
         model.fit(train_x, train_y)
         pred = model.predict(test_x)
-        self.print_score(model_name,train_y,train_x,test_y,pred, model )
+        self.print_score(model_name, train_y, train_x, test_y, pred, model)
 
     def polynomial(
             self,
@@ -115,7 +115,7 @@ class Algorithms(object):
 
         model2 = model.fit(train_x_poly, train_y)
         pred2 = model2.predict(test_x_poly)
-        self.print_score(model_name, train_y, train_x, test_y, pred2, model )
+        self.print_score(model_name, train_y, train_x, test_y, pred2, model)
 
     def linear(
             self,
@@ -128,7 +128,7 @@ class Algorithms(object):
         model = linear_model.LinearRegression()
         model.fit(train_x, train_y)
         pred = model.predict(test_x)
-        self.print_score(model_name,train_y,train_x,test_y,pred, model )
+        self.print_score(model_name, train_y, train_x, test_y, pred, model)
 
     def ridge(
             self,
@@ -141,7 +141,7 @@ class Algorithms(object):
         model = linear_model.Ridge(alpha=3)
         model.fit(train_x, train_y)
         pred = model.predict(test_x)
-        self.print_score(model_name,train_y,train_x,test_y,pred, model )
+        self.print_score(model_name, train_y, train_x, test_y, pred, model)
 
     def lasso(
             self,
@@ -154,7 +154,7 @@ class Algorithms(object):
         model = linear_model.Lasso()
         model.fit(train_x, train_y)
         pred = model.predict(test_x)
-        self.print_score(model_name,train_y,train_x,test_y,pred, model)
+        self.print_score(model_name, train_y, train_x, test_y, pred, model)
 
     def elasticnet(
             self,
@@ -167,14 +167,14 @@ class Algorithms(object):
         model = linear_model.ElasticNet(alpha=1)
         model.fit(train_x, train_y)
         pred = model.predict(test_x)
-        #print(pred)
-        self.print_score( model_name,train_y,train_x,test_y,pred, model )
+        # print(pred)
+        self.print_score(model_name, train_y, train_x, test_y, pred, model)
 
     def print_score(self, model_name, train_y, train_x, test_y, pred, model):
         try:
-            print(len(pred),len(test_y))
-            print(model_name+" Mean Squared Error: ", mean_squared_error(test_y, pred))
-            print(model_name+" R2 Score : ", r2_score(test_y, pred))
+            print(len(pred), len(test_y))
+            print(model_name + " Mean Squared Error: ", mean_squared_error(test_y, pred))
+            print(model_name + " R2 Score : ", r2_score(test_y, pred))
         except Exception:
             traceback.print_exc()
 
@@ -182,13 +182,9 @@ class Algorithms(object):
 if __name__ == '__main__':
     obj = Compute()
     (train_x, test_x, train_y, test_y) = obj.data_loader()
-    #print(len(train_x), len(train_y), len(test_x), len(test_y))
+    # print(len(train_x), len(train_y), len(test_x), len(test_y))
     obj2 = Algorithms("elasticnet", train_x, test_x, train_y, test_y)
     obj2 = Algorithms("svr", train_x, test_x, train_y, test_y)
     obj2 = Algorithms("lasso", train_x, test_x, train_y, test_y)
     obj2 = Algorithms("linear", train_x, test_x, train_y, test_y)
     obj2 = Algorithms("polynomial", train_x, test_x, train_y, test_y)
-
-
-
-
