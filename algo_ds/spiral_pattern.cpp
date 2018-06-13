9d0ffad86e7e94ee07a46d02ea703ba8e8c073ae
thefr33radical <imperial.gauntlet@gmail.com>
2018-06-13 15:23:59 +0530

    13jun18

--- algo_ds/spiral_pattern.cpp
+++ algo_ds/spiral_pattern.cpp
@@ -13,39 +13,45 @@ void compute()
     int m=3,n=4;
     vector<vector<int> > mat(m,vector<int>(n));
 
-   int top_left=0,top_right=n-1,bottom_left=0,bottom_right=m-1;
-   int mov_right=0,mov_down=0,mov_left=n-1,mov_up=m-1;
+  int bottom=m-1;
+  int right=n-1;
+  int left=0;
+  int top=0;
+ 
+
+   int mov_right=0,mov_left=right,mov_up=bottom,mov_down=0;
    int count =0;
 
-    while(top_left<top_right && bottom_right>top_left)
+    while(top<bottom || left<right)
     {
-        while(mov_right<top_right)
-        {   mat[top_left][mov_right]=count++;
+        while(mov_right<right)
+        {   mat[top][mov_right]=count++;
             mov_right++;
             
         }
-         while(mov_down<bottom_right)
-        {   mat[bottom_right][mov_down++]=count++;
+         while(mov_down<bottom)
+        {   mat[right][mov_down++]=count++;
            
         }
-         while(mov_left>bottom_left)
-        {   mat[bottom_left][mov_left--]=count++;
+         while(mov_left>left)
+        {   mat[bottom][mov_left--]=count++;
             
         }
 
-          while(mov_up>top_left)
-        {   mat[top_left][mov_up--]=count++;
+          while(mov_up>top)
+        {   mat[left][mov_up--]=count++;
             
         }
 
-        top_left++;
-        top_right--;
-        bottom_right--;
-        bottom_left--;
-        mov_right=top_left;
-        mov_down=top_right;
-        mov_left=bottom_right;
-        mov_up=bottom_left;
+        top++;
+        bottom--;
+        left++;
+        right--;
+
+        mov_right=left;
+        mov_down=top;
+        mov_left=right;
+        mov_up=bottom;
 
     }
 
