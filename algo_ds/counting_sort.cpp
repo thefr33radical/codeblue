38f56a91865d67ee5dfd8b05a7f11ded35bcccb0
thefr33radical <imperial.gauntlet@gmail.com>
2018-06-20 23:10:34 +0530

    20jun18

--- algo_ds/counting_sort.cpp
+++ algo_ds/counting_sort.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main()
 {
 
-    vector<int>arr{1,2,3,51,6,34,23};
+    vector<int>arr{1,2,3,9,6,3,2};
     vector<int>op(arr.size(),0);
-    double max_e=*max_element(arr.begin(),arr.end());
+    int max_e=*max_element(arr.begin(),arr.end());
     vector<int>count(max_e,0);
 
     for(int i=0;i<arr.size();i++)
@@ -13,11 +13,16 @@ int main()
         count[arr[i]]++;
     }
 
-
+for(int i:count)
+        cout<<i<<" ";
+     cout<<"\n";
     for(int i=1;i<count.size();i++)
     {
         count[i]=count[i-1]+count[i];
     }
+    for(int i:count)
+        cout<<i<<" ";
+     cout<<"\n";
 
      for(int i=0;i<arr.size();i++)
     {
@@ -26,6 +31,11 @@ int main()
        }
 
     for(int i:op)
+        cout<<i<<" ";
+    
+    
+    cout<<"\n";
+    for(int i:count)
         cout<<i<<" ";
 
     return 0;
