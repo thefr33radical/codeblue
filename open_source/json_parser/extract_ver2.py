4038cb06dfcf455081dfd9603e05bd6a7f7d49f6
thefr33radical <imperial.gauntlet@gmail.com>
2018-06-23 19:33:18 +0530

    23jun18

---
+++ open_source/json_parser/extract_ver2.py
@@ -0,0 +1,53 @@
+'''
+
+
+Script to convert nested json object to key value pairs. This will take care of multiple values in a single column
+Contributors : Thefreeradical, Mahendra
+
+
+Better optimized version using generators will be published soon.
+Result JSON OBJECT to CSV or DATAFRAME can be converted in one more step.
+ 1. Remove commented lines to load key value pairs into dictionary obj
+ 2. Use pandas library to convert dictionary to dataframe
+ 3. Convert Datframe to CSV
+
+
+'''    
+
+
+import json
+#obj=dict()
+pathname="/pathname/file.json"
+def extract(key,value):
+    if(type(value)==str):
+        result = "{'"+key+"' : '"+value+"'}"
+        print(result)
+        #obj[key]=value
+        return
+        
+    t = 0
+    if(type(value)==list):
+        for i in value:
+            extract(key+"_"+str(t),i)
+            t = t + 1
+        return
+        
+    if(type(value)==dict):
+        for i in value.keys():
+            if key:
+                key_i = key+"_"+i
+            else:
+                key_i = i
+            extract(key_i,value[i])
+        return
+        
+def compute():
+    with open(pathname)as f:
+        dat=f.read()
+        json_data=json.loads(dat)
+        d=dict(json_data)
+        extract(None,d)        
+        #print(obj)
+
+if __name__=="__main__":
+    compute()
