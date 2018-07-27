74b85d971d677092baa083fe0233d11919ffa8b5
thefr33radical <imperial.gauntlet@gmail.com>
2018-07-27 15:13:37 +0530

    27jul18

---
+++ algo_ds/print_matrix_pattern.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int m=5;
+	int n=6
+	vector<vector<int>>matrix(m,vector<int>(n,0));
+	
+	
+	for(auto i=0;i<m;i++)
+	{
+	{ for(auto j=0;j<n;j++)
+		
+	}
+	cout<<"\n";
+	}
+	
+	
+	for(auto i=0;i<m;i++)
+	{
+	{ for(auto j=0;j<n;j++)
+		
+	}
+	cout<<"\n";
+	}
+	
+	
+	return 0;
+}
