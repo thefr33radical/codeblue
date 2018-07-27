d362de5d2052710af2ba560032fe7ae961edc3a7
thefr33radical <imperial.gauntlet@gmail.com>
2018-07-27 15:34:27 +0530

    27jul18

--- algo_ds/print_matrix_pattern.cpp
+++ algo_ds/print_matrix_pattern.cpp
@@ -5,27 +5,72 @@ using namespace std;
 int main() {
 	// your code goes here
 	int m=5;
-	int n=6
+	int n=6;
 	vector<vector<int>>matrix(m,vector<int>(n,0));
 	
+/*
+0 0 0 0 0 
+0 0 0 0 
+0 0 0 
+0 0 
+0 
+
+	for(auto i=0;i<m;i++)
+	{
+	 
+		for(auto j=0;j<n;j++)
+		{
+			if(i<j)
+			{
+				cout<<matrix[i][j]<<" ";
+			}
+		}
+		cout<<"\n";
+	}
+	
+
+		for(auto i=0;i<m;i++)
+	{
+	 
+		for(auto j=0;j<n;j++)
+		{
+			if(i>j)
+			{
+				cout<<matrix[i][j]<<" ";
+			}
+		}
+		cout<<"\n";
+	}
+	
+
+	
+		for(auto i=0;i<m;i++)
+	{
+	 
+		for(auto j=0;j<n;j++)
+		{
+			if(i>j)
+			{
+				cout<<matrix[i][j]<<" ";
+			}
+		}
+		cout<<"\n";
+	}
+	
+*/	
+	
 	
 	for(auto i=0;i<m;i++)
 	{
-	{ for(auto j=0;j<n;j++)
+	{ for(auto j=i;j<n;j++)
 		
+	
+	cout<<i<<j<<" ";
 	}
 	cout<<"\n";
 	}
 	
-	
-	for(auto i=0;i<m;i++)
-	{
-	{ for(auto j=0;j<n;j++)
-		
-	}
-	cout<<"\n";
-	}
-	
+
 	
 	return 0;
 }
