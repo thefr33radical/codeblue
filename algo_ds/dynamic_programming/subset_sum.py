d82a82b722c396c3e500aa5cce1784aaa18e1c6a
thefr33radical <imperial.gauntlet@gmail.com>
2018-10-01 12:43:37 +0530

    1oct 18

--- algo_ds/dynamic_programming/subset_sum.py
+++ algo_ds/dynamic_programming/subset_sum.py
@@ -6,8 +6,10 @@
 
 ALgorithm to find if the sum N is possible from the subset { } 0f m numbers using DP
 
-1. Construct a matrix  MAT of m+1 X N+1 matrix. 
-2. Initialize  column to MAT[#][0]= { subset }, MAT[#][1] =1, because a 0 can be formed by empty subset. Mat[0][#] to 1~N.
+1. Construct a matrix  MAT of m+1 X N+1 matrix. matrix[i][0]=1;
+
+2. Initialize  column to MAT[i][0]= 1, MAT[0][i] =0, because a 0 can be formed by empty subset.
+
 3. for i=1 to m+1
 	for j=1 t0 n+1
 		if MAT[i]<MAT[j]
@@ -23,32 +25,22 @@ ALgorithm to find if the sum N is possible from the subset { } 0f m numbers usin
 
 
 
+def subset_sum():
+	array = [1,3,4,7]
+	total = 6
+	matrix = [ [0]*(total+1) for i in range(0,len(array)+1) ]
 
-
-
-N=8
-a= [3,4,5,1,2]
-
-mat =[ [0]*(N+1) for i in range(len(a)+1)]
-
-#mat= [row for i in range(0,(N))]
-
-for i in range(1,len(a)+1):
-    mat[i][0]=a[i-1]
-
-for i in range(1,N+1):
-    mat[0][i]=mat[0][i-1]+1
-
-
-for i in range(1,N+1):
-    for j in range(1,len(a)):
-
-        if a[i-1]<mat[0][i-1]:
-            mat[i][j]=mat[i-1][j-i]
-
-
-
-print(mat)
-
-
+	for i in range(0,len(matrix)):
+		matrix[i][0]=1
+		
+	for i in range(1,len(matrix)):
+		for j in range(1,len(matrix[i])):
+			if j<array[i-1]:
+				matrix[i][j]=matrix[i-1][j]
+			else:
+				matrix[i][j]=matrix[i-1][j] or matrix[i-1][j - array[i-1]]
+				
+				
+	print(matrix)
+	print(matrix[len(matrix)-1][len(matrix[0])-1])
 
