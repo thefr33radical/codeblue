06937bab16416094a80e9b18d665b2f8cc9cf770
thefr33radical <imperial.gauntlet@gmail.com>
2018-10-18 15:34:58 +0530

    18oct18

--- algo_ds/dynamic_programming/ugly_numbers.py
+++ algo_ds/dynamic_programming/ugly_numbers.py
@@ -3,10 +3,18 @@
 Given n find the nth ugly numbers.
 """
 
+def notprime(number):
+    for j in range(6, number-1):
+        if number % j == 0:
+            print("false")
+            return False
+    print("true")
+    return True
+
 
 if __name__=="__main__":
    # no= int(input())
-    no =8
+    no =11
 
     if no == 1:
         print("1")
@@ -15,25 +23,21 @@ if __name__=="__main__":
     if no ==3:
         print("3")
     if no ==4:
+        print("4")
+    if no ==5:
         print("5")
 
     number =6
-    i=5
-    while i <= no:
-        if number %2 == 0 and number%3==0 and number%5 ==0:
-            is_ugly=1
-            for j in range(6,number):
-                if number%j ==0:
-                    is_ugly =0
-                    break
-            if is_ugly ==1:
-                i+=1
-                number+=1
-            else:
-                number+=1
+    i=6
+    while i <= no :
+
+        if (number %2 == 0) or (number%3==0) or (number%5 ==0) and notprime(number):
+            print(i, number)
+            i+=1
+            number+=1
 
         else:
             number+=1
 
-    print(number)
+    #print(i-1,number-1)
 
