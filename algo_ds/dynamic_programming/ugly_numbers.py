b61bc34fdc0dcfda4420428be24151b53062d062
thefr33radical <imperial.gauntlet@gmail.com>
2018-10-18 16:04:38 +0530

    18oct18

--- algo_ds/dynamic_programming/ugly_numbers.py
+++ algo_ds/dynamic_programming/ugly_numbers.py
@@ -3,6 +3,7 @@
 Given n find the nth ugly numbers.
 """
 
+import pickle
 def isprime(number):
     for i in range(2,number):
         if number%i ==0:
@@ -20,31 +21,50 @@ def notprime(number):
 
 
 if __name__=="__main__":
-   # no= int(input())
-    no =11
+    # no= int(input())
+    no = 11
+    with open("storage_array.txt", "w") as f:
+        storage = pickle.loads(f)
 
-    if no == 1:
-        print("1")
-    if no ==2 :
-        print("2")
-    if no ==3:
-        print("3")
-    if no ==4:
-        print("4")
-    if no ==5:
-        print("5")
+    if (len(storage) > no+1):
+        print(storage[no][2])
+    else:
+        storage = []
+        storage.append((1, 1))
+        storage.append((2, 2))
+        storage.append((3, 3))
+        storage.append((4, 4))
+        storage.append((5, 5))
 
-    number =6
-    i=6
-    while i <= no :
+        if no == 1:
+            print("1")
+        if no ==2 :
+            print("2")
+        if no ==3:
+            print("3")
+        if no ==4:
+            print("4")
+        if no ==5:
+            print("5")
 
-        if ((number %2 == 0) or (number%3==0) or (number%5 ==0)) and notprime(number):
-            print(i, number)
-            i+=1
-            number+=1
+        number =6
+        i=6
+        storage =[]
+        storage.append((1,1))
 
-        else:
-            number+=1
+        while i <= no :
 
-    #print(i-1,number-1)
+            if ((number %2 == 0) or (number%3==0) or (number%5 ==0)) and notprime(number):
+                print(i, number)
+                storage.append((i, number))
+                i+=1
+                number+=1
+
+            else:
+                number+=1
+
+        with open("storage_array.txt","w") as f:
+            pickle.dumps(storage,f)
+
+        #print(i-1,number-1)
 
