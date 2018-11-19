3016b0124a5db7c2fe871521c321e73e80f21157
thefr33radical <imperial.gauntlet@gmail.com>
2018-11-19 18:02:05 +0530

    19nov18

--- algo_ds/dynamic_programming/min_cost_path.py
+++ algo_ds/dynamic_programming/min_cost_path.py
@@ -36,6 +36,22 @@ def trace_min_cost_path(cost):
     :return:
     """
     
+    path=[]
+    for i in range(cost):
+        path.append([0]*len(cost[i]))
+        
+    path[0][0]=cost[0][0]
+    for i in range(1, len(cost[0])):
+        path[i][0]+=cost[i][0]+path[i-1][0]
+        
+    for j in range(1,len(cost)):
+        path[0][j] = path[0][j-1] + cost[0][j]
+        
+    for i in range(1,len(cost)):
+        for j in range(1,len(cost[i])):
+            path[i][j] = min(cost[i][j-1],cost[i-1][j-1],cost[i-1][j])
+        
+    
 
 if __name__=="__main__":
     cost = [[1, 2, 3],         [4, 8, 2],         [1, 5, 3]]
