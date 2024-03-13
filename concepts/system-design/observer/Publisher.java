d034dcd887e95de76cc26c4bbf8c1e6d1ceb11d4
Gowtham Nayak <imperial.gauntlet@gmail.com>
2024-03-12 20:25:03 -0500

    Update Publisher.java

--- concepts/system-design/observer/Publisher.java
+++ concepts/system-design/observer/Publisher.java
@@ -1,1 +1,67 @@
 
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+
+
+public interface Publisher
+{
+	
+	public void register(Observer o);
+	public void deRegister(Observer o);
+	public void notify();
+	
+}
+
+public interface Observer
+{
+
+	public void update(int var1, int var2);
+}
+
+public class WeatherStation implements Publisher
+{
+	private List<Observer> observers;
+	
+	Publishers()
+	{this.observers = new ArrayList<>();
+	}
+	
+	@overrirde
+	public void register(Observer o)
+	{
+		observers.add(o);
+	}
+	
+	
+	@overrirde
+	public void deregister(Observer o)
+	{
+		int index = observers.indexOf(o);
+		if(index >=0)
+		{
+			observers.remove(o);
+		}
+	}
+	
+	@override
+	public void notify(){
+		
+		// for i in observsers  notify call o.update
+	}
+	
+	
+}
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	}
+}
