7e22058ecb1bc3c62117cc33fb7a3a81cea04976
Gowtham Nayak <imperial.gauntlet@gmail.com>
2025-02-24 18:36:19 -0600

    Add reading materials 21.02.25

---
+++ scratch/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    
+    public static void main(String args[])
+    {
+
+     
+    for(int i=1; i<=5;i++)
+    {
+        for(int j=1;j<=5;j++)
+        {
+            if(i>=j)
+            System.out.print(" ");
+            else
+                System.out.print(j);
+        }
+        System.out.print("\n");
+    }
+
+    System.out.println("fg");
+    }
+}
